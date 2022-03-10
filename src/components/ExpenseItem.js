935990cbfd004f3eb889fc2a3fdcd71c98376898
cghayes <60023846+cghayes@users.noreply.github.com>
2022-03-09 22:49:42 -0600

    Add files via upload
    add data and card container

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,18 @@
 import React from "react";
 
-import "./ExpenseItem.css";
+// import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+import Card from "./Card";
 
-function ExpenseItem() {
-
+function ExpenseItem(props) {
   return (
-    <div className="expense-item">
-      <div>March 8, 2022</div>
+    <Card className="expense-item">
+      <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$96.33</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
---
+++ src/constants.js
@@ -0,0 +1,26 @@
+export const expenses = [
+  {
+    id: "e1",
+    title: "Plane ticket",
+    amount: 453.86,
+    date: new Date(2022, 2, 8),
+  },
+  {
+    id: "e2",
+    title: "Airbnb",
+    amount: 768.64,
+    date: new Date(2022, 2, 14),
+  },
+  {
+    id: "e3",
+    title: "New hiking shoes",
+    amount: 145.12,
+    date: new Date(2022, 2, 8),
+  },
+  {
+    id: "e4",
+    title: "Concert tickets",
+    amount: 75.55,
+    date: new Date(2022, 5, 12),
+  },
+];
